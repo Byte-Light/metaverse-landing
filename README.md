d3869a3ff094e1354b84d684c2d115fdc345d7d3
SheikhULAB <tosibalaptop86@gmail.com>
2024-12-01 17:24:29 +0600

    metaverse landing page

---
+++ README.md
@@ -0,0 +1,14 @@
+# Welcome to METAVERSE
+
+🚀 **Metaverse** landing page using React, Next.js, Framer-motion and Tailwindcss
+
+![Metaverse](./public/metaverse.png)
+
+## Technologies Used
+
+- JavaScript
+- React
+- Next.js
+- Tailwindcss
+- Framer-motion
+# metaverse-landing
---
+++ app/head.js
@@ -0,0 +1,10 @@
+const Head = () => (
+  <>
+    <title>Metaverse</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="'Metaverse' landing page using React, Next.js, Framer-motion and Tailwindcss" />
+    <link rel="icon" href="/favicon.ico" />
+  </>
+);
+
+export default Head;
